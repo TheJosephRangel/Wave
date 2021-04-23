ead80a96828a634528877480421193879cfbfb32
Joseph Rangel <jrangel13@islander.tamucc.edu>
2021-04-22 20:14:14 -0500

    Added to Home Page and Rewards Page Layout. Added Data to Both Layouts as Well.

--- app/src/main/java/com/example/wave/MainActivity.java
+++ app/src/main/java/com/example/wave/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.wave;
 
-import androidx.annotation.NonNull;
+//import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
